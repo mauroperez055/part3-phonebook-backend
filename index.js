d556c1510d46f8a8918870d4658f68d68ed98676
mauroperez055 <mauroperez055@gmail.com>
2025-09-08 16:36:47 -0300

    Agrego ejercicio 3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
     { 
       "id": 1,
@@ -48,6 +50,31 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id);
 
   response.status(204).end();
+})
+
+const generateId = () => {
+  const id = Math.floor((Math.random() * 10000) + 1); //  
+  return id;
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+  console.log(body);
+
+  if (!body.name) {
+    return response.status(404).json({
+      error: 'content missing'
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person);
+  response.json(person);
 })
 
 const PORT = 3001;
