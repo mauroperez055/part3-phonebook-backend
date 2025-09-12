285d868345bb50af72134fa5a864fbdb22dafd44
mauroperez055 <mauroperez055@gmail.com>
2025-09-12 12:51:51 -0300

    Agrego ejercicio 3.8

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
 
+const app = express();
 app.use(express.json()); 
-app.use(morgan('tiny'));
+
+morgan.token('body', (request) => {
+  return request.method === 'POST' ? JSON.stringify(request.body) : ' ';  
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
     { 
