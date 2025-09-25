b116ed5d622a28bc47f33e7d67f633ede7d46c8e
mauroperez055 <mauroperez055@gmail.com>
2025-09-25 15:39:00 -0300

    Actualizo backend

--- models/person.js
+++ models/person.js
@@ -1,28 +1,26 @@
 const mongoose = require("mongoose");
 
-if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
-}
-
-const password = process.argv[2];
-
-const url = `mongodb+srv://maurodb:${password}@cluster0.p4bfxiy.mongodb.net/phonebook? 
-  retryWrites=true&w=majority&appName=Cluster0`;
-
 mongoose.set("strictQuery", false);
 
-mongoose.connect(url);
+const url = process.env.MONGODB_URI;
+
+console.log('connecting to', url);
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB');
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message);
+  })
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
 });
 
-const Person = mongoose.model("Person", personSchema);
-
 // si solo se pasa la contraseña como argumento, conecta y muestra la lista de contactos agregados
-if (process.argv.length === 3) {
+/* if (process.argv.length === 3) {
   Person
     .find({})
     .then((result) => {
@@ -46,4 +44,6 @@ if (process.argv.length === 3) {
 } else {
   console.log("Uso: node mongo.js <password> [<'name'> <number>]");
   mongoose.connection.close();
-}
+} */
+
+module.exports = mongoose.model("Person", personSchema);
